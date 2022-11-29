e25e565d2042b540ea8b18c6157e65e1e2668c6a
김도영 <kdzero0317@gmail.com>
2022-11-29 15:11:35 +0900

    refactor (Token) : JwtService, JwtServiceImpl 코드 수정

--- noticeBoard/src/main/java/toyproject/noticeBoard/global/jwt/service/JwtService.java
+++ noticeBoard/src/main/java/toyproject/noticeBoard/global/jwt/service/JwtService.java
@@ -3,6 +3,7 @@ package toyproject.noticeBoard.global.jwt.service;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 public interface JwtService {
     String createAccessToken(String username);
@@ -12,14 +13,17 @@ public interface JwtService {
 
     void destroyRefreshToken(String username);
 
-    void sendToken(HttpServletResponse response, String accessToken, String refreshToken) throws IOException;
+    void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken);
+    void sendAccessToken(HttpServletResponse response, String accessToken);
 
-    String extractAccessToken(HttpServletRequest request) throws IOException;
+    Optional<String> extractAccessToken(HttpServletRequest request);
 
-    String extractRefreshToken(HttpServletRequest request) throws  IOException;
+    Optional<String> extractRefreshToken(HttpServletRequest request);
 
-    String extractUsername(String accessToken);
+    Optional<String> extractUsername(String accessToken);
 
     void setAccessTokenHeader(HttpServletResponse response, String accessToken);
     void setRefreshTokenHeader(HttpServletResponse response, String refreshToken);
+
+    boolean isTokenValid(String token);
 }
--- noticeBoard/src/main/java/toyproject/noticeBoard/global/jwt/service/JwtServiceImpl.java
+++ noticeBoard/src/main/java/toyproject/noticeBoard/global/jwt/service/JwtServiceImpl.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import toyproject.noticeBoard.domain.member.repository.MemberRepository;
@@ -24,6 +25,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Setter(value = AccessLevel.PRIVATE)
+@Slf4j
 public class JwtServiceImpl implements JwtService {
 
     @Value("${jwt.secret}")
@@ -43,7 +45,6 @@ public class JwtServiceImpl implements JwtService {
     private static final String BEARER = "Bearer ";
 
     private final MemberRepository memberRepository;
-    private final ObjectMapper objectMapper;
 
 
     @Override
@@ -82,8 +83,7 @@ public class JwtServiceImpl implements JwtService {
     }
 
     @Override
-    public void sendToken(HttpServletResponse response, String accessToken, String refreshToken) throws IOException {
-        response.setContentType("application/json; charset=UTF-8");
+    public void sendAccessAndRefreshToken(HttpServletResponse response, String accessToken, String refreshToken) {
         response.setStatus(HttpServletResponse.SC_OK);
 
         setAccessTokenHeader(response, accessToken);
@@ -93,32 +93,44 @@ public class JwtServiceImpl implements JwtService {
         tokenMap.put(ACCESS_TOKEN_SUBJECT, accessToken);
         tokenMap.put(REFRESH_TOKEN_SUBJECT, refreshToken);
 
-        String token = objectMapper.writeValueAsString(tokenMap);
-
-        response.getWriter().write(token);
     }
 
     @Override
-    public String extractAccessToken(HttpServletRequest request) throws IOException {
-        return Optional.ofNullable(request.getHeader(accessHeader))
-                .map(accessToken -> accessToken.replace(BEARER, ""))
-                .orElse(null);
+    public void sendAccessToken(HttpServletResponse response, String accessToken) {
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        setAccessTokenHeader(response, accessToken);
+
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put(ACCESS_TOKEN_SUBJECT, accessToken);
+    }
+
+
+    @Override
+    public Optional<String> extractAccessToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(accessHeader)).filter(
+                    accessToken -> accessToken.startsWith(BEARER)
+                ).map(accessToken -> accessToken.replace(BEARER, ""));
     }
 
     @Override
-    public String extractRefreshToken(HttpServletRequest request) throws IOException {
-        return Optional.ofNullable(request.getHeader(refreshHeader))
-                .map(refreshToken -> refreshToken.replace(BEARER, ""))
-                .orElse(null);
+    public Optional<String> extractRefreshToken(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(refreshHeader)).filter(
+                    refreshToken -> refreshToken.startsWith(BEARER)
+                ).map(refreshToken -> refreshToken.replace(BEARER, ""));
     }
 
     @Override
-    public String extractUsername(String accessToken) {
-        return JWT.require(Algorithm.HMAC512(secret))
-                .build()
-                .verify(accessToken)
-                .getClaim(USERNAME_CLAIM)
-                .asString();
+    public Optional<String> extractUsername(String accessToken) {
+        try {
+            return Optional.ofNullable(JWT.require(Algorithm.HMAC512(secret)).build()
+                    .verify(accessToken)
+                    .getClaim(USERNAME_CLAIM)
+                    .asString());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Optional.empty();
+        }
     }
 
     @Override
@@ -129,5 +141,16 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
         response.setHeader(refreshHeader, refreshToken);
+    }
+
+    @Override
+    public boolean isTokenValid(String token) {
+        try {
+            JWT.require(Algorithm.HMAC512(secret)).build().verify(token);
+            return true;
+        } catch (Exception e) {
+            log.error("유효하지 않은 Token 입니다.", e.getMessage());
+            return false;
+        }
     }
 }
